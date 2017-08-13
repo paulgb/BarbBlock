f75d5c45754b94d8c29424efaa5505c808ed5115
Paul Butler <paulgb@gmail.com>
2017-08-13 17:33:52 -0400

    add blacklist templates and generation script

--- Makefile
+++ Makefile
@@ -1,4 +1,7 @@
 
+blacklists/% : src/generate_blacklist.py blacklist.json templates/%.tmpl
+	python $^ $@
+
 package :
 	cd extension; zip barbblock.zip -r ./ -x *.git*
 	mv extension/barbblock.zip ./
---
+++ src/generate_blacklist.py
@@ -0,0 +1,32 @@
+import argparse
+import os
+import json
+
+import jinja2
+
+
+def run_template_engine(blacklist_file, template_file, output_file):
+    template_path, template_file = os.path.split(template_file)
+
+    with open(blacklist_file) as bf:
+        context = json.load(bf)
+
+    result = jinja2.Environment(
+        loader=jinja2.FileSystemLoader(template_path or './')
+    ).get_template(template_file).render(context)
+
+    with open(output_file, 'w') as of:
+        of.write(result)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('blacklist')
+    parser.add_argument('template')
+    parser.add_argument('outfile')
+    args = parser.parse_args()
+    run_template_engine(args.blacklist, args.template, args.outfile)
+
+
+if __name__ == '__main__':
+    main()
