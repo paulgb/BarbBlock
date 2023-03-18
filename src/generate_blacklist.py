0d9dd646a3f5910896c91e9a8247755eb6f1d464
moongazer07 <myboymohammed@gmail.com>
2023-03-18 16:25:31 -0400

    Update generate_blacklist.py

--- src/generate_blacklist.py
+++ src/generate_blacklist.py
@@ -6,13 +6,13 @@ import yaml
 import jinja2
 
 
-def run_template_engine(blacklist_file, template_file, output_file):
+def run_template_engine(blocklist_file, template_file, output_file):
     template_path, template_file = os.path.split(template_file)
 
-    with open(blacklist_file) as bf:
+    with open(blocklist_file) as bf:
         context = yaml.load(bf)
     
-    context['domains'] = [d for blockset in context['blacklist']
+    context['domains'] = [d for blockset in context['blocklist']
                           for d in blockset['domains']]
 
     result = jinja2.Environment(
@@ -25,11 +25,11 @@ def run_template_engine(blacklist_file, template_file, output_file):
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument('blacklist')
+    parser.add_argument('blocklist')
     parser.add_argument('template')
     parser.add_argument('outfile')
     args = parser.parse_args()
-    run_template_engine(args.blacklist, args.template, args.outfile)
+    run_template_engine(args.blocklist, args.template, args.outfile)
 
 
 if __name__ == '__main__':
